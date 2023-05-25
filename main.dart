28232ebd55d1c88c7128cba9bce345488436f526
meninojohnny <120663121+meninojohnny@users.noreply.github.com>
2023-05-25 08:40:30 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,36 +1,33 @@
 import 'package:flutter/material.dart';
-
 void main(){
-  runApp(AppWidget(title: 'Joao vitor'));
+  runApp(MyApp());
 }
 
-class AppWidget extends StatelessWidget{
-  final String title;
-  
-  const AppWidget({Key key, this.title = ''}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      theme: ThemeData.light(),
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget{
-  @override
-  State<HomePage> creatState() {
-    return HomePageState();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
   
-}
-
-class HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Joao  vitor')),
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World!!')
+        ,), 
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(color: Colors.green),
+      ),
     );
   }
 }

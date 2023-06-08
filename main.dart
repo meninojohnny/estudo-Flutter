37c3ac8a7f0c4f3f729f36f0e3672b135ee8b642
meninojohnny <120663121+meninojohnny@users.noreply.github.com>
2023-06-08 19:09:05 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,33 +1,89 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-  
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello World!!')
-        ,), 
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(color: Colors.green),
+        title: Text('Projeto1'),
       ),
+      body: Column(children: [
+        Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Center(
+            child: Text(
+              'Hello world',
+              style: TextStyle(
+               color: Colors.white,
+               fontSize: 30,
+              ),
+            )
+          ),
+        ),
+        Row(
+          children: [
+            Container(
+              color: Colors.red, 
+              height: 200, 
+              width: 200,
+              child: Center(
+                child: Text(
+                  'Bloco 1',
+                  style: TextStyle(color: Colors.yellow, fontSize: 20),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.yellow, 
+              height: 200, 
+              width: 200,
+              child: Center(
+                child: Text(
+                  'Bloco 2',
+                  style: TextStyle(color: Colors.red, fontSize: 20),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ]),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add),
+          label: 'item 1',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_card),
+          label: 'item 2',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_alert),
+          label: 'item 3',
+        ),
+      ]),
     );
   }
 }

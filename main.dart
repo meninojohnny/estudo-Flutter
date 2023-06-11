193ccf0e45efcadf4fe2f80a0b3ccdac33ec48c9
meninojohnny <120663121+meninojohnny@users.noreply.github.com>
2023-06-11 13:19:40 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,7 +23,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
   PageController _pageController = PageController();
   int indexPage = 0;
 
@@ -34,125 +32,112 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Projeto1'),
       ),
-      body: PageView(
-        controller: _pageController, 
-        children: [
-          PageOne(),
-          PageTwo(),
-          PageTree(),
-        ]),
-      bottomNavigationBar: BottomNavigationBar(
-         currentIndex: indexPage,
-        onTap: (int index) {
-          setState(() {
-            indexPage = index;
-          });
-          _pageController.jumpToPage(index);
-          },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'item 1',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_card),
-            label: 'item 2',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_alert),
-            label: 'item 3',
-          ),
+      body: PageView(controller: _pageController, children: [
+        PageOne(),
+        PageTwo(),
+        PageTree(),
       ]),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: indexPage,
+          onTap: (int index) {
+            setState(() {
+              indexPage = index;
+            });
+            _pageController.jumpToPage(index);
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'item 1',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_card),
+              label: 'item 2',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_alert),
+              label: 'item 3',
+            ),
+          ]),
     );
   }
 }
 
 class PageOne extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Column(
-            children: [
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Center(
-                  child: Text(
-                    'Page 1',
-                    style: TextStyle(
-                     color: Colors.white,
-                     fontSize: 30,
-                    ),
-                  )
-                ),
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        height: 200,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+        ),
+        child: Center(
+            child: Text(
+          'Page 1',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+        )),
+      ),
+      Row(
+        children: [
+          Container(
+            color: Colors.red,
+            height: 200,
+            width: 200,
+            child: Center(
+              child: Text(
+                'Page 2',
+                style: TextStyle(color: Colors.white, fontSize: 30),
               ),
-            Row(
-              children: [
-                Container(
-                  color: Colors.red, 
-                  height: 200, 
-                  width: 200,
-                  child: Center(
-                    child: Text(
-                      'Page 2',
-                      style: TextStyle(
-                        color: Colors.white, 
-                        fontSize: 30
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.yellow, 
-                  height: 200, 
-                  width: 200,
-                  child: Center(
-                    child: Text(
-                      'Page 3',
-                      style: TextStyle(
-                        color: Colors.white, 
-                        fontSize: 30
-                      ),
-                    ),
-                  ),
-                ),
-              ],
             ),
-          ]);
+          ),
+          Container(
+            color: Colors.yellow,
+            height: 200,
+            width: 200,
+            child: Center(
+              child: Text(
+                'Page 3',
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ]);
   }
 }
 
 class PageTwo extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-              color: Colors.red,
-              child: 
-              Center(
-                child: Text(
-                  'Page 2',
-                   style: TextStyle(color: Colors.white, fontSize: 30),
-                ),
-              ),
-            );
+      color: Colors.red,
+      child: Center(
+        child: Text(
+          'Page 2',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+      ),
+    );
   }
 }
-  
+
 class PageTree extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-              color: Colors.red,
-              child: 
-              Center(
-                child: Text(
-                  'Page 3',
-                   style: TextStyle(color: Colors.white, fontSize: 30),
-                ),
-              ),
-            );
+      color: Colors.red,
+      child: Center(
+        child: Text(
+          'Page 3',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+      ),
+    );
   }
 }
-  

88e9b9d16b51fcbbe51a8d55f00072b2a7fff642
meninojohnny <120663121+meninojohnny@users.noreply.github.com>
2023-06-12 20:03:42 -0300

    Create counter.dart

---
+++ counter.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+  print('Hello World');
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Counter());
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _Counter createState() => _Counter();
+}
+
+class _Counter extends State<Counter> {
+  int contador = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contador'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Clique para aumentar o contador',
+              style: TextStyle(fontSize: 15),
+            ),
+            Text(
+              '$contador',
+              style: TextStyle(fontSize: 35),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton:
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+        FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              contador++;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+        FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () {
+            setState(() {
+              if (contador > 0) {
+                contador--;
+              }
+            }
+           );
+          },
+          child: Icon(Icons.remove),
+        ),
+      ]
+     ),
+    );
+  }
+}

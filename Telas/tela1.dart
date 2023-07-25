b2825171e1419e9ef35b829449e973ef1845d7b3
meninojohnny <120663121+meninojohnny@users.noreply.github.com>
2023-07-25 15:36:41 -0300

    Update tela1.dart

--- Telas/tela1.dart
+++ Telas/tela1.dart
@@ -12,48 +12,62 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromRGBO(218, 215, 205, 1),
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        backgroundColor: Color.fromRGBO(237, 255, 217, 1),
         //brightness: Brightness.dark, // ajusta o brilho do conteudo da appBar
         elevation: 0, // controla a sombra da appBar
-        leading: IconButton( // leading cria um botão do lado esquerdo da appBar
+        leading: IconButton(
+          // leading cria um botão do lado esquerdo da appBar
           icon: Icon(Icons.menu, color: Colors.black),
-          onPressed: () {print('Hello world');},
+          onPressed: () {
+            print('Hello');
+          },
         ),
       ), //appBAr
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
+          //crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
-                color: Color.fromRGBO(229, 212, 206, 1),
-                borderRadius: BorderRadius.vertical(bottom: Radius.circular(15)), // arredonda apenas os cantos escolhidos
+                color: Color.fromRGBO(237, 255, 217, 1),
+                borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(
+                        15)), // arredonda apenas os cantos escolhidos
               ),
               padding: EdgeInsets.all(20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Find Your', style: TextStyle(color: Colors.black, fontSize: 25),),
+                  Text(
+                    'Find Your',
+                    style: TextStyle(color: Colors.black, fontSize: 25),
+                  ),
                   SizedBox(height: 3),
-                  Text('Inspiration', style: TextStyle(color: Colors.black, fontSize: 40),),
+                  Text(
+                    'Inspiration',
+                    style: TextStyle(color: Colors.black, fontSize: 40),
+                  ),
                   SizedBox(height: 20),
                   Container(
                     decoration: BoxDecoration(
-                      color: Color.fromRGBO(244, 243, 243, 1),
+                      color: Color.fromRGBO(218, 215, 205, 1),
                       borderRadius: BorderRadius.circular(15),
                     ),
                     child: TextField(
                       decoration: InputDecoration(
                         border: InputBorder.none, // tira a borda do textField
-                        prefixIcon: Icon(Icons.search, color: Colors.black,),
-                        hintText: 'Search you lookink for', // define o texto dentro do field
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                        hintText:
+                            'Search you lookink for', // define o texto dentro do field
                         hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
                       ),
                     ), // TextField
@@ -62,18 +76,89 @@ class HomePageState extends State {
                 ],
               ), // Column
             ), // Container
-            SizedBox(height: 10),
+            SizedBox(height: 20),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20), // coloca padding apenas horizontalmente
+              padding: EdgeInsets.symmetric(
+                  horizontal: 20), // coloca padding apenas horizontalmente
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Promo Today', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                  Text(
+                    'Promo Today',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis
+                          .horizontal, // o conteudo será exibido horizontalemnte
+                      children: [
+                        promoCard(Colors.orange),
+                        promoCard(Colors.red),
+                        promoCard(Colors.green),
+                      ],
+                    ), // ListView
+                  ), // Container
+                  SizedBox(height: 15),
+                  Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blue,
+                    ),
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: EdgeInsets.all(15),
+                        child: Text(
+                          'Best Design',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
                 ],
-              ),
+              ), // Column
             ), // Padding
           ],
-        ), // Collumn
+        ), // Column
       ), // SafeArea
+    );
+  }
+
+  Widget promoCard(MaterialColor color) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 3,
+      child: Container(
+        margin: EdgeInsets.only(right: 15), // cria uma margem do lado direito
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(20),
+          /*image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/images/one.jpg'),
+          ),*/
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+              // faz um degrade
+              begin: Alignment.bottomRight, // direção do degrade
+              stops: [0.1, 0.9], // n]ao entendi muito bem ("-")
+              colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.1),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
